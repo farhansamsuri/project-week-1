71f0cedbb64c5fcc2d53ece38f6aff3249323f82
Farhan <mdfarhansamsuri@gmail.com>
2022-04-25 18:03:43 +0800

    Day 1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # project-week-1
 Project : Pomodoro
+
+1. Basic layout with html and css
+
+
--- pomodoro.js
+++ pomodoro.js
@@ -1,3 +1,38 @@
 // Countdown Timer https://www.educative.io/edpresso/how-to-create-a-countdown-timer-using-javascript
 // To Do List https://www.w3schools.com/howto/howto_js_todolist.asp
+// Special thanks to : Joy, 
 
+
+//newInput function creates new task to the to-do list.
+const newInput = () => {
+
+    //creates a new li, then append
+    let li = document.createElement("li");
+    let input = document.querySelector("#input");
+    let target = document.createTextNode(input);
+    let todo = [];
+    li.appendChild(target);
+
+    if (input.value === '') {
+        alert("Add something!");
+    } else {
+            document.querySelector("#ul").appendChild(li);
+    }
+
+    li.innerText = input.value;
+    let span = document.createElement('span');
+    let exit = document.createTextNode("\u00D7"); // \u00D7 for a nicer 'x'.
+    span.className = "close";
+    span.appendChild(exit);
+    li.appendChild(span);
+    
+ 
+    //Still don't understand - please understand first before proceeding.
+/*     close = document.getElementsByClassName("close");
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+        let div = this.parentElement;
+          div.style.display = "none";
+        }
+    } */
+}
